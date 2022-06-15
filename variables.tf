047d2f7c675261d04f0de37d77cb43a3565a356a
Richard <richwats@cisco.com>
2022-06-15 17:04:09 +1000

    2nd FSO instance

---
+++ variables.tf
@@ -0,0 +1,30 @@
+## IWO Collector Variables ##
+variable "iwo_chart_url" {
+  type = string
+}
+
+## AppD Variables ##
+variable "appd_account_name" {
+  type = string
+}
+
+variable "appd_otel_api_key" {
+  type = string
+}
+
+variable "appd_account_key" {
+  type = string
+}
+
+variable "appd_account_username" {
+  type = string
+}
+
+variable "appd_account_password" {
+  type = string
+}
+
+# ## ThousandEyes Variables ##
+# variable "te_token" {
+#   type = string
+# }
